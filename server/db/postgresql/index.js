b312febdda4a286a84ef66696362814c2540b673
Elvin Ly <elvinly323@gmail.com>
2025-11-01 22:30:08 -0400

    Finish sequelize db manager

--- server/db/postgresql/index.js
+++ server/db/postgresql/index.js
@@ -7,11 +7,12 @@ class SequelizeManager extends DatabaseManager {
         this.sequelize = new Sequelize(process.env.POSTGRESQL_URI);
         this.User = null;
         this.Playlist = null;
+        this.initializeModels();
     }
 
     async initializeModels() {
         //User
-        this.User = sequelize.define('User', {
+        this.User = this.sequelize.define('User', {
             id: {
                 type: DataTypes.INTEGER,
                 primaryKey: true,
@@ -39,7 +40,7 @@ class SequelizeManager extends DatabaseManager {
         });
 
         //Playlist
-        this.Playlist = sequelize.define('Playlist', {
+        this.Playlist = this.sequelize.define('Playlist', {
             id: {
                 type: DataTypes.INTEGER,
                 primaryKey: true,
@@ -63,19 +64,19 @@ class SequelizeManager extends DatabaseManager {
         });
 
         //define the associations
-        this.User.hasMany(Playlist, {
+        this.User.hasMany(this.Playlist, {
             foreignKey: 'userId',
             as: 'playlists'
         });
 
-        this.Playlist.belongsTo(User, {
+        this.Playlist.belongsTo(this.User, {
             foreignKey: 'userId'
         });
     }
 
     async connect() {
         await this.sequelize.authenticate()
-        console.log('Connected to MongoDB');
+        console.log('Connected to Postgres');
 
     }
 
@@ -109,35 +110,148 @@ class SequelizeManager extends DatabaseManager {
 
     async createPlaylist(playlistData, userID) {
         try {
-            
+            const user = await this.User.findByPk(userID);
+            console.log("user found: " + JSON.stringify(user));
+
+            playlistData.userId = userID;
+            const playlist = await this.Playlist.create(playlistData);
+            console.log("playlist: " + JSON.stringify(playlist));
+
+            return playlist;
+
         } catch (error) {
             console.error('Error creating playlist:', error);
             return null;
         }
+    }
+
+    async deletePlaylist(playlistID, userID) {
+        const playlist = await this.Playlist.findByPk(playlistID);
+        console.log("playlist found: " + JSON.stringify(playlist));
+
+        if (!playlist) {
+            throw new Error('Playlist not found!');
+        }
+
+        // DOES THIS LIST BELONG TO THIS USER?
+        const user = await this.User.findOne({ where: { email: playlist.ownerEmail } });
+        console.log("user._id: " + user.id);
+        console.log("userID: " + userID);
+
+        if (user.id == userID) {
+            console.log("correct user!");
+            await playlist.destroy();
+            return {};
+        } else {
+            console.log("incorrect user!");
+            throw new Error("authentication error");
+        }
+    }
+
+    async getPlaylistById(playlistID, userID) {
+        try {
+            const playlist = await this.Playlist.findByPk(playlistID);
+
+            if (!playlist) {
+                throw new Error('Playlist not found');
+            }
+
+            console.log("Found list: " + JSON.stringify(playlist));
+
+            // Verify ownership
+            const user = await this.User.findOne({ where: { email: playlist.ownerEmail } });
+            console.log("user._id: " + user.id);
+            console.log("userID: " + userID);
+
+            if (user.id.toString() === userID.toString()) {
+                console.log("correct user!");
+                return playlist;
+            } else {
+                console.log("incorrect user!");
+                throw new Error('authentication error');
+            }
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    }
+
+    async getPlaylistPairs(userID) {
+        try {
+            console.log("find user with id " + userID);
+            const user = await this.User.findByPk(userID);
+
+            console.log("find all Playlists owned by " + user.email);
+            const playlists = await this.Playlist.findAll({
+                where: { ownerEmail: user.email },
+                attributes: ['id', 'name'] //we only need the id and name
+            })
+            console.log("found Playlists: " + JSON.stringify(playlists));
+
+            if (!playlists) {
+                console.log("!playlists.length");
+                return { success: false, error: 'Playlists not found' }
+            }
+            else {
+                console.log("Send the Playlist pairs");
+                // PUT ALL THE LISTS INTO ID, NAME PAIRS
+                let pairs = [];
+                for (let key in playlists) {
+                    let list = playlists[key];
+                    let pair = {
+                        _id: list.id,
+                        name: list.name
+                    };
+                    pairs.push(pair);
+                }
+                return pairs
+            }
+        } catch (err) {
+            console.error(err)
+            throw err
+        }
+    }
+
+    async updatePlaylist(playlistID, userID, updateData) {
+        try {
+            const playlist = await this.Playlist.findByPk(playlistID);
+            console.log("playlist found: " + JSON.stringify(playlist));
 
 
-        // const playlist = new Playlist(playlistData);
-        // console.log("playlist: " + playlist.toString());
-        // if (!playlist) {
-        //     return null;
-        // }
+            // DOES THIS LIST BELONG TO THIS USER?
+            const user = await this.User.findOne({ where: { email: playlist.ownerEmail } });
+            console.log("user._id: " + user.id);
+            console.log("req.userId: " + userID);
+            if (user.id == userID) {
+                console.log("correct user!");
+                console.log("updateData.name: " + updateData.name);
 
-        // this.User.findOne({ _id: userID }, (err, user) => {
-        //     console.log("user found: " + JSON.stringify(user));
-        //     user.playlists.push(playlist._id);
-        //     user
-        //         .save()
-        //         .then(() => {
-        //             playlist
-        //                 .save()
-        //                 .then(() => {
-        //                     return playlist;
-        //                 })
-        //                 .catch(error => {
-        //                     return null;
-        //                 })
-        //         });
-        // })
-        // return playlist;
+                playlist.name = updateData.name;
+                playlist.songs = updateData.songs;
+                const updatedPlaylist = await playlist.save()
+
+                console.log("SUCCESS!!!");
+                return updatedPlaylist
+
+            }
+            else {
+                console.log("incorrect user!");
+                return { success: false, description: "authentication error" }
+            }
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
+    }
+
+    async getPlaylists() {
+        const playlists = await this.Playlist.findAll();
+        if (!playlists.length) {
+            return { success: false, error: `Playlists not found` }
+        }
+        return playlists
     }
 }
+
+const dbManager = new SequelizeManager();
+module.exports = dbManager;
